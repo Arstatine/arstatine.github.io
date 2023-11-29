db6fe7ffc98a30f08c8f804add493d919cc99c72
Xenon <x.vergara2201@gmail.com>
2023-11-29 23:22:31 +0800

    project build

--- src/views/index.jsx
+++
@@ -1,6 +0,0 @@
-import Home from './home/Home';
-import About from './about/About';
-import Projects from './projects/Projects';
-import Skills from './skills/Skills';
-
-export { Home, About, Projects, Skills };
